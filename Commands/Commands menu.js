8ef9babde5f5615de00f66d76618368df25f137f
mredgar2008 <zombieedgar56@gmail.com>
2025-07-25 01:23:59 +0300

    Update Commands menu.js

--- Commands/Commands menu.js
+++ Commands/Commands menu.js
@@ -1,1 +1,23 @@
+```javascript
+module.exports = {
+  name: "menu",
+  description: "Onesha orodha ya amri",
+  category: "general",
+  async run({ sock, msg }) {
+    await sock.sendMessage(msg.from, {
+      text: `🤖 *MR. EDGAR-XMD BOT MENU*
+
+📌 Commands:
+!menu
+!owner
+!ping
+!sticker
+!tts
+!ai
+!groupinfo
+!help`,
+    });
+  },
+};
+``
 
